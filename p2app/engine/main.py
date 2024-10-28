007d5beea45060ae9d9ef6789e65d43828f3e219
Peter Le <dedsnailyghost13@gmail.com>
2024-10-28 00:38:06 -0700

    Events for Searching Entries

--- p2app/engine/main.py
+++ p2app/engine/main.py
@@ -23,7 +23,7 @@ class Engine:
 
     def __init__(self):
         """Initializes the engine"""
-        pass
+        self.connection = None
 
 
     def process_event(self, event):
@@ -34,14 +34,14 @@ class Engine:
         # Database #
         #----------#
 
-        if (isinstance(event, QuitInitiatedEvent)):
+        if isinstance(event, QuitInitiatedEvent):
             yield EndApplicationEvent()
 
-        elif (isinstance(event, OpenDatabaseEvent)):
+        elif isinstance(event, OpenDatabaseEvent):
             try:
                 file_extension = os.path.splitext(event.path())[-1]
                 if file_extension == '.db':
-                    sqlite3.connect(event.path())
+                    self.connection = sqlite3.connect(event.path())
                     yield DatabaseOpenedEvent(event.path())
                 else:
                     raise sqlite3.Error
@@ -52,5 +52,81 @@ class Engine:
         # Application #
         #-------------#
 
-        elif (isinstance(event, CloseDatabaseEvent)):
+        elif isinstance(event, CloseDatabaseEvent):
             yield DatabaseClosedEvent()
+
+        #-----------#
+        # Continent #
+        #-----------#
+
+        elif isinstance(event, StartContinentSearchEvent):
+            # gets entered parameters
+            continent_code = event.continent_code()
+            name = event.name()
+
+            # generates WHERE statement
+            where_statement = f'WHERE continent_code = \'{continent_code}\' AND name = \'{name}\''
+            if not continent_code:
+                where_statement = f'WHERE name = \'{name}\''
+            elif not name:
+                where_statement = f'WHERE continent_code = \'{continent_code}\''
+
+            # retrieves matching continents
+            cursor = self.connection.execute(f'''SELECT * FROM continent {where_statement};''')
+            for continent in cursor:
+                yield ContinentSearchResultEvent(Continent(*continent))
+
+        #---------#
+        # Country #
+        #---------#
+
+        elif isinstance(event, StartCountrySearchEvent):
+            # gets entered parameters
+            country_code = event.country_code()
+            name = event.name()
+
+            # generates WHERE statement
+            where_statement = f'WHERE country_code = \'{country_code}\' AND name = \'{name}\''
+            if not country_code:
+                where_statement = f'WHERE name = \'{name}\''
+            elif not name:
+                where_statement = f'WHERE country_code = \'{country_code}\''
+
+            # retrieves matching countries
+            cursor = self.connection.execute(f'''SELECT * FROM country {where_statement};''')
+            for country in cursor:
+                yield CountrySearchResultEvent(Country(*country))
+
+        #--------#
+        # Region #
+        #--------#
+
+        elif isinstance(event, StartRegionSearchEvent):
+            # gets entered parameters
+            region_code = event.region_code()
+            local_code = event.local_code()
+            name = event.name()
+
+            # scenario with all parameters entered
+            columns = [region_code, local_code, name]
+            params = [f'region_code = \'{region_code}\'',
+                      f'local_code = \'{local_code}\'',
+                      f'name =\'{name}\'']
+
+            # removes unentered parameters
+            for i in range(2, -1, -1):
+                if not columns[i]:
+                    del params[i]
+
+            # generates WHERE statement
+            where_statement = ''
+            for param in params:
+                if not where_statement:
+                    where_statement += 'WHERE ' + param
+                else:
+                    where_statement += 'AND ' + param
+
+            # retrieves matching regions
+            cursor = self.connection.execute(f'''SELECT * FROM region {where_statement};''')
+            for region in cursor:
+                yield RegionSearchResultEvent(Region(*region))
